91de91d32e4d549569bce13e14c30fcf6f53b3a9
HouJun11220033 <1144724015@qq.com>
2016-09-17 19:10:59 +0800

    Handler

--- ProgressBarHandler/src/com/example/MainActivity.java
+++ ProgressBarHandler/src/com/example/MainActivity.java
@@ -26,27 +26,54 @@ public class MainActivity extends Activity {
 
 		@Override
 		public void onClick(View v) {
-			bar.setVisibility(v.VISIBLE);
+			bar.setVisibility(View.VISIBLE);
+			updateBarHandler.post(updateThread);
 
 		}
 
 	}
 
-	Handler updateBarHandler =new Handler(){
-		
-		public void handleMessage(Message message)
-		{
+	Handler updateBarHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message message) {
 			bar.setProgress(message.arg1);
-			Bundle bundle=message.getData();
-			updateBarHandler.post(update)
-			
+			Bundle bundle = message.getData();
+			// post方法：往里面传入线程
+			updateBarHandler.post(updateThread);
+			System.out.println("test---->" + bundle.getString("test"));
+
 		}
-		
+
 	};
-	Runnable updateThread=new Runnable(){int i=0;
 
-	@Override public void run(){System.out.println("Begin Thread"+i);i=i+10;Message message=updateBarHandler.obtainMessage();message.arg1=i;
+	// int i = 0;
+	Runnable updateThread = new Runnable() {
+		int i = 0;
 
-	}};
+		public void run() {
+
+			System.out.println("Begin Thread" + i);
+			i = i + 10;
+			Message message = updateBarHandler.obtainMessage();
+			message.arg1 = i;
+			Bundle bundle = new Bundle();
+			bundle.putString("test", "test bundle");
+			message.setData(bundle);
+			try {
+				Thread.sleep(100);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			if (i > 100) {
+				updateBarHandler.removeCallbacks(updateThread);
+				System.out.println(">>>>>>>>");
+			} else {
+				updateBarHandler.handleMessage(message);
+				System.out.println("<<<<<<<<");
+			}
+
+		}
+	};
 
 }
