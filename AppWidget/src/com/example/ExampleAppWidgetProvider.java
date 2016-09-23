42340e906cb1460b94d3d8c29412fd5759424678
HouJun11220033 <1144724015@qq.com>
2016-09-23 11:22:21 +0800

    RemoteViews对象方法

--- AppWidget/src/com/example/ExampleAppWidgetProvider.java
+++ AppWidget/src/com/example/ExampleAppWidgetProvider.java
@@ -3,6 +3,7 @@ package com.example;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
@@ -13,11 +14,20 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
-		super.onReceive(context, intent);
 		String action = intent.getAction();
+
 		if (UPDATE_ACTION.equals(action)) {
+			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.example_appwidget);
+			remoteViews.setImageViewResource(R.id.imageId, R.drawable.ku);
+			remoteViews.setTextViewText(R.id.widgetTextId, "test");
+			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+			ComponentName componentName = new ComponentName(context, ExampleAppWidgetProvider.class);
+			appWidgetManager.updateAppWidget(componentName, remoteViews);
 
 			System.out.println("onReceive---->" + UPDATE_ACTION);
+		} else {
+
+			super.onReceive(context, intent);
 		}
 	}
 
@@ -27,8 +37,10 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
 		// Intent intent = new Intent(context, TargetActivity.class);
 		Intent intent = new Intent();
 		intent.setAction(UPDATE_ACTION);
-		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
-		PendingIntent pendingIntent2 = PendingIntent.getActivity(context, 0, intent, 0);
+
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, -1, intent, 0);
+		// PendingIntent pendingIntent2 = PendingIntent.getActivity(context, 0,
+		// intent, 0);
 		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.example_appwidget);
 		// remoteViews.setOnClickPendingIntent(R.id.widgetButton,
 		// pendingIntent2);
