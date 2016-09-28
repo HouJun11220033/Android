ff7c2f7259afe26dca435de9d753cbf9468af062
HouJun11220033 <1144724015@qq.com>
2016-09-28 23:35:26 +0800

    布局文件

--- Mp3Player/src/com/mp3player/Mp3ListActivity.java
+++ Mp3Player/src/com/mp3player/Mp3ListActivity.java
@@ -27,7 +27,7 @@ public class Mp3ListActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.main);
 	}
 
 	@Override
